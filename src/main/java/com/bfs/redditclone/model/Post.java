ede3db9ecb9fbf62c11d720923d5129a7390f319
Ian Li <yli@beaconfireinc.com>
2020-07-17 02:46:53 -0400

    add User and Subreddit

--- src/main/java/com/bfs/redditclone/model/Post.java
+++ src/main/java/com/bfs/redditclone/model/Post.java
@@ -1,4 +1,32 @@
 package com.bfs.redditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty or Null")
+    private String postName;
+    @Nullable
+    private String url;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount = 0;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
 }
