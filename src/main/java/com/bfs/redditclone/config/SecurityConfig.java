0b1b2ff7f897b949be0bd7890cf97a042e67936e
Ian Li <yli@beaconfireinc.com>
2020-08-10 02:39:28 -0400

    modifying spring security config and adding UserDetailsServiceImpl

--- src/main/java/com/bfs/redditclone/config/SecurityConfig.java
+++ src/main/java/com/bfs/redditclone/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.bfs.redditclone.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         // bearer token, in our case jwt, is not vulnerable to csrf attacks
@@ -19,6 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
